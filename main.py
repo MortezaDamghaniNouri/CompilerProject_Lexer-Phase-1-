7516847c84791a57cfd64d1d8d28506b9a7de859
Morteza-Damghani <mortezadamghani77@gmail.com>
2020-10-29 08:01:11 -0700

    PROJECT COMPLETELY FINISHED.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from lexer import Lexer
 
 object1 = Lexer().build()
-file = open("MyTest.txt")
-input_text = file.read()
-file.close()
-object1.input(input_text)
+inputFile = open("test1.txt")
+inputText = inputFile.read()
+inputFile.close()
+outputFile = open("test1 result.txt", "w")
+object1.input(inputText)
 while True:
     tok = object1.token()
     if not tok:
         break
     print(tok)
+    outputFile.write(str(tok)+"\n")
+outputFile.close()
